8c5ea0f28f824f12e7909aac53d3486e7688ae14
Shimi Razilov <shimi.r@phantomauto.com>
2021-01-14 19:27:19 +0200

    merged lol into dev

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-const Bot = require("./bot");
-const express = require('express')
+const Bot = require('./bot');
+const ChatStorage = require('./services/chat-storage');
+const express = require('express');
 
 const app = express()
 const port = 8081
@@ -20,26 +21,30 @@ function spawnBot() {
   const Example = require("./commands/Example");
   const Poll = require("./commands/Poll");
   const Lol = require("./commands/Lol")
-  const Lucy = new Bot({name: 'Lucy'});
-
+  const Lucy = require("./commands/Lucy")
+  
+  const chatStorage = new ChatStorage();
+  const bot = new Bot({name: 'Lucy', storage: chatStorage});
   console.log('Starting telegram bot..');
 
   // Register new commands like this line: 
-  Lucy.registerCommand(Example.keywords, Example.parser);
-  Lucy.registerCommand(Common.keywords, Common.parser);
-  Lucy.registerCommand(NBAHandler.keywords, NBAHandler.parser);
-  Lucy.registerCommand(Poll.keywords, Poll.parser);
-  Lucy.registerCommand(Lol.keywords, Lol.parser);
+  bot.registerCommand(Lucy.keywords, Lucy.parser);
+  bot.registerCommand(Example.keywords, Example.parser);
+  bot.registerCommand(Common.keywords, Common.parser);
+  bot.registerCommand(NBAHandler.keywords, NBAHandler.parser);
+  bot.registerCommand(Poll.keywords, Poll.parser);
+  bot.registerCommand(Lol.keywords, Lol.parser);
+  bot.registerCommand(Lucy.keywords, Lucy.parser);
 
   // Inline example
-  Lucy.registerCommand(['hey', 'hi', 'hello'], (incomingMessage, bot, keyMatches) => {
+  bot.registerCommand(['hey', 'hi', 'hello'], (incomingMessage, api, keyMatches) => {
     // got "/lucy ...hey/hi/hello..."
     return `${keyMatches[0]} back`;
   });
 
   console.log('Registered all commands.');
 
-  Lucy.start();
+  bot.start();
 }
 
 
